cbabb5ee1f2dbe4741713873f9b510e8b24c1e2b
Chezo <dev.chezo@gmail.com>
2022-02-02 16:27:12 +0100

    Az adataim valahogy sosem kerülnek bele a datába function-on kívül

--- scripts/fileIO.js
+++ scripts/fileIO.js
@@ -1,3 +1,11 @@
+let data;
+
+fetch("/data/sample.json")
+    .then(response => response.json())
+    .then(x => { data = x });
+
+console.log(data)
+
 function download(filename, text) {
     var pom = document.createElement('a');
     pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
@@ -13,34 +21,21 @@ function download(filename, text) {
 }
 
 
-function readJsonFile(file) {
-    let bufferData = fs.readFileSync(file)
-    let stData = bufferData.toString()
-    let data = JSON.parse(stData)
-    return data
-}
 
-fetch("./data/sample.json")
-    .then(response => response.json())
-    .then(data => { console.log(data) })
-
-function onFileLoad(elementId, event) {
-    console.log(event.target.result)
-        // document.getElementById(elementId).innerText = event.target.result;
-}
-
-function onChooseFile(event, onLoadFileHandler) {
+function onChooseFile(event) {
     if (typeof window.FileReader !== 'function')
         throw ("The file API isn't supported on this browser.");
     let input = event.target;
-    if (!input)
-        throw ("The browser does not properly implement the event object");
-    if (!input.files)
-        throw ("This browser does not support the `files` property of the file input.");
-    if (!input.files[0])
-        return undefined;
+
     let file = input.files[0];
     let fr = new FileReader();
-    fr.onload = onLoadFileHandler;
-    fr.readAsText(file);
+
+
+    fr.onload = function() {
+        data = fr.result; //itt még megvan az adat
+
+    };
+    console.log(data); //itt már nincs :D
+    //fr.readAsText(file);
+
 }
