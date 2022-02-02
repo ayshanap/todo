16576468a9344598580facd509f913a74ee1a1cb
Chezo <dev.chezo@gmail.com>
2022-02-02 11:08:36 +0100

    :)

---
+++ scripts/Todo.js
@@ -0,0 +1,13 @@
+class Todo {
+
+    constructor(name, description, created, deadline, priority) {
+        this.name = name;
+        this.description = description;
+        this.created = created;
+        this.deadline = deadline;
+        this.priority = priority;
+
+    }
+
+
+}
---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+let todo1 = new Todo("todo1", "todo1 leírás", "2020", "2022", "1");
+let todo2 = new Todo("todo2", "todo2 leírás", "2020", "2022", "1");
+let todo3 = new Todo("todo3", "todo3 leírás", "2020", "2022", "1");
+
+
+
+//egy "listából" az elemeket belerakja egy row divbe értékekkel együtt
+//(később fileból)
+//RENDEZÉSEK
+//todo hozzáadás
+//todo törlés
+//done/undone
+//edit
+//import
+//export
