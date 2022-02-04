bfb61bb58c7ef1905442d77fc8a65b1a13ec64e5
KovacsLaszloZsolt <57332094+KovacsLaszloZsolt@users.noreply.github.com>
2022-02-04 11:56:47 +0000

    Update TodoList.js

--- scripts/TodoList.js
+++ scripts/TodoList.js
@@ -1,7 +1,6 @@
 import Todo from "./Todo.js";
 
 class TodoList {
-<<<<<<< HEAD
   constructor(list, date) {
     this.list = list;
     this.date = date;
@@ -65,25 +64,6 @@ class TodoList {
       ? new Todo(task, this).init(completedTodosCtn)
       : new Todo(task, this).init(activeTodosCtn);
   }
-=======
-    constructor(list) {
-        this.list = list;
-    }
-
-    init() {
-        this.renderList(this.list);
-    }
-
-    renderList(listObj) {
-        const todoListCtn = document.querySelector(".todo-list-ctn");
-        todoListCtn.innerText = "";
-        listObj.forEach((task) => this.addNewTask(task, todoListCtn));
-    }
-
-    addNewTask(task, ctn) {
-        ctn.appendChild(new Todo(task, this).init());
-    }
->>>>>>> a04644533daedf9a3c177d8015ec311349dab5ca
 
     removeTask(id) {
         const taskId = this.list.findIndex((task) => task.id === id);
@@ -95,82 +75,7 @@ class TodoList {
     }
 }
 
-<<<<<<< HEAD
-const data = [
-  {
-    id: 1,
-    name: "todo1",
-    description: "todo1 leírás",
-    createAt: "2020.02.02",
-    deadline: "2022.03.01",
-    priority: 1,
-    taskDone: true,
-  },
-  {
-    id: 5,
-    name: "todo5",
-    description: "todo5 leírás",
-    createAt: "2020.02.07",
-    deadline: "2022.03.30",
-    priority: 1,
-    taskDone: false,
-  },
-  {
-    id: 2,
-    name: "todo2",
-    description: "todo2 leírás",
-    createAt: "2020.02.01",
-    deadline: "2022.03.07",
-    priority: 2,
-    taskDone: true,
-  },
-  {
-    id: 3,
-    name: "todo3",
-    description: "todo3 leírás",
-    createAt: "2020.02.04",
-    deadline: "2022.03.12",
-    priority: 3,
-    taskDone: false,
-  },
-];
-
 const myTodo = new TodoList(data, new Date());
-=======
-const myTodo = new TodoList([{
-        id: 1,
-        name: "todo1",
-        description: "todo1 leírás",
-        createAt: "2020.02.02",
-        deadline: "2022.03.01",
-        priority: 1,
-    },
-    {
-        id: 5,
-        name: "todo5",
-        description: "todo5 leírás",
-        createAt: "2020.02.07",
-        deadline: "2022.03.30",
-        priority: 1,
-    },
-    {
-        id: 2,
-        name: "todo2",
-        description: "todo2 leírás",
-        createAt: "2020.02.01",
-        deadline: "2022.03.07",
-        priority: 2,
-    },
-    {
-        id: 3,
-        name: "todo3",
-        description: "todo3 leírás",
-        createAt: "2020.02.04",
-        deadline: "2022.03.12",
-        priority: 3,
-    },
-]);
->>>>>>> a04644533daedf9a3c177d8015ec311349dab5ca
 
 myTodo.init();
 myTodo.orderBy("priority");
