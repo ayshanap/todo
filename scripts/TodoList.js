c83efcafc70a0c9ef1a30bc26dc9f06e769e5763
KovacsLaszloZsolt <57332094+KovacsLaszloZsolt@users.noreply.github.com>
2022-02-04 12:14:22 +0000

    Update TodoList.js

--- scripts/TodoList.js
+++ scripts/TodoList.js
@@ -75,6 +75,45 @@ class TodoList {
     }
 }
 
+const data = [
+  {
+    id: 1,
+    name: "todo1",
+    description: "todo1 leírás",
+    createAt: "2020.02.02",
+    deadline: "2022.03.01",
+    priority: 1,
+    taskDone: true,
+  },
+  {
+    id: 5,
+    name: "todo5",
+    description: "todo5 leírás",
+    createAt: "2020.02.07",
+    deadline: "2022.03.30",
+    priority: 1,
+    taskDone: false,
+  },
+  {
+    id: 2,
+    name: "todo2",
+    description: "todo2 leírás",
+    createAt: "2020.02.01",
+    deadline: "2022.03.07",
+    priority: 2,
+    taskDone: true,
+  },
+  {
+    id: 3,
+    name: "todo3",
+    description: "todo3 leírás",
+    createAt: "2020.02.04",
+    deadline: "2022.03.12",
+    priority: 3,
+    taskDone: false,
+  },
+];
+
 const myTodo = new TodoList(data, new Date());
 
 myTodo.init();
