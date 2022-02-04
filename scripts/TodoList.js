c85a0780a9ae67ac42ae2e841a4cd3df7a6a710a
Kovacs Laszlo Zsolt <laszlo.zs.kovacs@gmail.com>
2022-02-04 11:28:28 +0000

    todoList Judit htmlje szerint

--- scripts/TodoList.js
+++ scripts/TodoList.js
@@ -1,22 +1,68 @@
 import Todo from "./Todo.js";
 
 class TodoList {
-  constructor(list) {
+  constructor(list, date) {
     this.list = list;
+    this.date = date;
   }
 
   init() {
+    const date = document.querySelector("#date");
+    date.innerHTML = `${this.getDate()}<br />${this.getToday()}`;
+
+    const newTodoBtn = document.querySelector("#newBtn");
+    newTodoBtn.addEventListener("click", () => {});
+
+    this.setNumberOfTaskLeft(this.list);
+
     this.renderList(this.list);
   }
 
-  renderList(listObj) {
-    const todoListCtn = document.querySelector(".todo-list-ctn");
-    todoListCtn.innerText = "";
-    listObj.forEach((task) => this.addNewTask(task, todoListCtn));
+  getDate() {
+    const year = this.date.getFullYear();
+    const month = this.date.getMonth() + 1;
+    const day = this.date.getDate();
+    const setTwoDigit = (value) => {
+      return value < 10 ? "0" + value : value;
+    };
+    return `${year}.${setTwoDigit(month)}.${setTwoDigit(day)}`;
   }
 
-  addNewTask(task, ctn) {
-    ctn.appendChild(new Todo(task, this).init());
+  getToday() {
+    return this.date.toLocaleDateString("en-En", { weekday: "long" });
+  }
+
+  setNumberOfTaskLeft(todoList) {
+    const getNumberOfTaskLeft = () => {
+      if (!todoList.length) {
+        return 0;
+      }
+
+      return todoList.filter((item) => item.taskDone === false).length;
+    };
+
+    const todoItemsLeft = document.querySelector("#todo-items-left");
+    todoItemsLeft.innerText = `You have ${getNumberOfTaskLeft(
+      todoList
+    )} pending items.`;
+  }
+
+  renderList(listObj) {
+    const activeTodosCtn = document.querySelector("#active-todos-ctn");
+    const completedTodosCtn = document.querySelector("#completed-todos-ctn");
+    activeTodosCtn.innerText = "";
+    completedTodosCtn.innerText = "";
+    listObj.forEach((task) =>
+      this.createTask(task, activeTodosCtn, completedTodosCtn)
+    );
+  }
+
+  createTask(task) {
+    const activeTodosCtn = document.querySelector("#active-todos-ctn");
+    const completedTodosCtn = document.querySelector("#completed-todos-ctn");
+    task.taskDone
+      ? new Todo(task, this).init(completedTodosCtn)
+      : new Todo(task, this).init(activeTodosCtn);
   }
 
   removeTask(id) {
@@ -29,7 +75,7 @@ class TodoList {
   }
 }
 
-const myTodo = new TodoList([
+const data = [
   {
     id: 1,
     name: "todo1",
@@ -37,6 +83,7 @@ const myTodo = new TodoList([
     createAt: "2020.02.02",
     deadline: "2022.03.01",
     priority: 1,
+    taskDone: true,
   },
   {
     id: 5,
@@ -45,6 +92,7 @@ const myTodo = new TodoList([
     createAt: "2020.02.07",
     deadline: "2022.03.30",
     priority: 1,
+    taskDone: false,
   },
   {
     id: 2,
@@ -53,6 +101,7 @@ const myTodo = new TodoList([
     createAt: "2020.02.01",
     deadline: "2022.03.07",
     priority: 2,
+    taskDone: true,
   },
   {
     id: 3,
@@ -61,8 +110,11 @@ const myTodo = new TodoList([
     createAt: "2020.02.04",
     deadline: "2022.03.12",
     priority: 3,
+    taskDone: false,
   },
-]);
+];
+
+const myTodo = new TodoList(data, new Date());
 
 myTodo.init();
 myTodo.orderBy("priority");
