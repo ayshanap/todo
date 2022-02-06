56e2f3019603a6bb6000d1a053d624202f7e4730
Kovacs Laszlo Zsolt <laszlo.zs.kovacs@gmail.com>
2022-02-06 09:55:53 +0000

    clean my branch

--- scripts/Todo.js
+++ scripts/Todo.js
@@ -1,71 +1,111 @@
-import TodoList from "./TodoList.js";
-
 class Todo {
   constructor(task, list) {
-    (this.id = task.id),
-      (this.name = task.name),
-      (this.description = task.description),
-      (this.createAt = task.createAt),
-      (this.deadline = task.deadline),
-      (this.priority = task.priority),
-      (this.todoList = list);
+    (this.task = task), (this.todoList = list);
+    // (this.id = task.id),
   }
 
-  init() {
-    const newTask = document.createElement("div");
-    newTask.classList.add("todo-item");
-    newTask.dataset.id = this.id;
-    newTask.classList.add("row");
+  init(ctn) {
+    const todoCtn = document.createElement("div");
+    todoCtn.classList.add("todo");
+    todoCtn.classList.add("active"); //later not needed
+    todoCtn.dataset.id = this.task.id;
+    ctn.prepend(todoCtn);
 
-    newTask.innerHTML = `
-                    <div class="inRow">${this.name}</div>
-                    <div class="inRow">${this.description}</div>
-                    <div class="inRow">${this.createAt}</div>
-                    <div class="inRow">${this.deadline}</div>
-                    <div class="inRow">${this.priority}</div>
-        `;
-
-    const interactCtn = document.createElement("div");
-    newTask.appendChild(interactCtn);
-
-    const createBtn = (title) => {
-      const btn = document.createElement("span");
-      btn.classList.add(`${title}-btn`);
-      btn.innerText = title;
-      return btn;
-    };
-
-    const editBtn = createBtn("edit");
-    interactCtn.appendChild(editBtn);
-    editBtn.addEventListener("click", () => {
-      //edit modal apeare, fields filled with task data
-    });
-
-    const delBtn = createBtn("delete");
-    interactCtn.appendChild(delBtn);
-    delBtn.addEventListener("click", () => {
-      const todoList = [...document.querySelectorAll(".todo-item")];
-      todoList.forEach((task) => console.log(task.dataset.id, this.id));
-      const itemForDelete = todoList.find(
-        (task) => parseFloat(task.dataset.id) === this.id
-      );
-      itemForDelete.remove();
-      this.todoList.removeTask(this.id);
-    });
+    const todoMain = document.createElement("div");
+    todoMain.classList.add("todo-main");
+    todoMain.classList.add("active"); //later not needed
+    todoMain.dataset.completed = this.task.taskDone;
+    todoCtn.appendChild(todoMain);
 
     const checkInput = document.createElement("input");
     checkInput.setAttribute("type", "checkbox");
+    checkInput.classList.add("check");
+
+    if (this.task.taskDone) {
+      checkInput.checked = true;
+    } else {
+      checkInput.checked = false;
+    }
 
     checkInput.addEventListener("click", (e) => {
+      const taskInDom = this.getTaskElement(this.task.id);
+      console.log(taskInDom);
+      const taskInObj = this.todoList.list.find(
+        (elem) => elem.id === this.task.id
+      );
+      taskInDom.remove();
+
+      let taskCtn;
       if (e.target.checked) {
-        // task done
+        taskInObj.taskDone = true;
+        taskCtn = document.querySelector(".container-completed");
       } else {
-        //   task not done
+        taskInObj.taskDone = false;
+        taskCtn = document.querySelector(".container-active");
+      }
+
+      this.init(taskCtn);
+
+      this.todoList.setNumberOfTaskLeft(this.todoList.list);
+    });
+
+    todoMain.appendChild(checkInput);
+
+    const taskName = document.createElement("h2");
+    taskName.classList.add("todoName");
+    taskName.innerText = this.task.name;
+    todoMain.appendChild(taskName);
+
+    const delBtn = document.createElement("a");
+    delBtn.classList.add("delBtn");
+    delBtn.innerText = "X";
+    todoMain.appendChild(delBtn);
+
+    delBtn.addEventListener("click", () => {
+      const itemForDelete = this.getTaskElement(this.task.id);
+      itemForDelete.remove();
+      this.todoList.removeTask(this.task.id);
+      if (!this.task.taskDone) {
+        this.todoList.setNumberOfTaskLeft(this.todoList.list);
       }
     });
 
-    interactCtn.appendChild(checkInput);
-    return newTask;
+    const todoDescr = document.createElement("div");
+    todoDescr.classList = "todo-descr active";
+    todoCtn.appendChild(todoDescr);
+
+    const description = document.createElement("p");
+    description.innerText = this.task.description;
+
+    todoDescr.appendChild(description);
+
+    const detailsCtn = document.createElement("div");
+    detailsCtn.classList.add("details-mobile");
+
+    detailsCtn.innerHTML = `<ul>
+               <li>Prio: ${this.task.priority}</li>
+               <li>Created: ${this.todoList.getDate(
+                 new Date(this.task.createAt)
+               )}</li>
+               <li>Deadline: ${this.todoList.getDate(
+                 new Date(this.task.deadline)
+               )}</li>
+             </ul>`;
+
+    todoDescr.appendChild(detailsCtn);
+
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("editBtn");
+    editBtn.innerText = "Edit";
+
+    editBtn.addEventListener("click", () => {});
+    detailsCtn.appendChild(editBtn);
+    return todoMain;
+  }
+
+  getTaskElement(taskId) {
+    const todoList = [...document.querySelectorAll(".todo")];
+    return todoList.find((task) => parseFloat(task.dataset.id) === taskId);
   }
 }
 
--- scripts/TodoList.js
+++ scripts/TodoList.js
@@ -1,21 +1,79 @@
 import Todo from "./Todo.js";
 
 class TodoList {
-  constructor(list) {
+  constructor(list, date, lastID) {
     this.list = list;
+    this.date = date;
+    this.lastID = lastID;
   }
 
   init() {
+    console.log();
+    const date = document.querySelector("#date");
+    date.innerHTML = `${this.getDate(this.date)}<br />${this.getToday()}`;
+
+    const newTodoBtns = document.querySelectorAll(".newBtn");
+    newTodoBtns.forEach((btn) =>
+      btn.addEventListener("click", () => {
+        const newTodoModal = document.querySelector(".modal-new");
+
+        if (newTodoModal.classList.value.includes("inactive")) {
+          newTodoModal.classList.remove("inactive");
+        }
+      })
+    );
+
+    this.setNumberOfTaskLeft(this.list);
+
     this.renderList(this.list);
+
+    this.initFilters();
+    this.initNewTodoModal();
+  }
+
+  getDate(date) {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const setTwoDigit = (value) => {
+      return value < 10 ? "0" + value : value;
+    };
+    return `${year}.${setTwoDigit(month)}.${setTwoDigit(day)}`;
+  }
+
+  getToday() {
+    return this.date.toLocaleDateString("en-En", { weekday: "long" });
+  }
+
+  setNumberOfTaskLeft(todoList) {
+    const getNumberOfTaskLeft = () => {
+      if (!todoList.length) {
+        return 0;
+      }
+
+      return todoList.filter((item) => item.taskDone === false).length;
+    };
+
+    const todoItemsLeft = document.querySelector("#todo-items-left");
+    todoItemsLeft.innerText = `You have ${getNumberOfTaskLeft(
+      todoList
+    )} pending items.`;
   }
 
   renderList(listObj) {
-    const todoListCtn = document.querySelector(".todo-list-ctn");
-    listObj.forEach((task) => this.addNewTask(task, todoListCtn));
+    const activeTodosCtn = document.querySelector(".container-active");
+    const completedTodosCtn = document.querySelector(".container-completed");
+    activeTodosCtn.innerText = "";
+    completedTodosCtn.innerText = "";
+    listObj.forEach((task) => this.createTask(task));
   }
 
-  addNewTask(task, ctn) {
-    ctn.appendChild(new Todo(task, this).init());
+  createTask(task) {
+    const activeTodosCtn = document.querySelector(".container-active");
+    const completedTodosCtn = document.querySelector(".container-completed");
+    task.taskDone
+      ? new Todo(task, this).init(completedTodosCtn)
+      : new Todo(task, this).init(activeTodosCtn);
   }
 
   removeTask(id) {
@@ -23,47 +81,134 @@ class TodoList {
     this.list.splice(taskId, 1);
   }
 
-  orderBy(category) {
-    const categoryValues = this.list.map((task) => task[category]);
+  // orderBy(category) {
+  //   const categoryValues = this.list.map((task) => task[category]);
+  // }
+
+  initFilters() {
+    const filterBtns = document.querySelectorAll(".filterBtn");
+
+    filterBtns.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        const datasetValue = btn.dataset.taskdone;
+
+        const activeTodosCtn = document.querySelector(".container-active");
+        const completedTodosCtn = document.querySelector(
+          ".container-completed"
+        );
+
+        const removeInactiveClass = (ctn) => {
+          if (ctn.classList.value.includes("inactive")) {
+            ctn.classList.remove("inactive");
+          }
+        };
+
+        const addInactiveClass = (ctn) => {
+          if (!ctn.classList.value.includes("inactive")) {
+            ctn.classList.add("inactive");
+          }
+        };
+
+        if (datasetValue === "") {
+          removeInactiveClass(activeTodosCtn);
+          removeInactiveClass(completedTodosCtn);
+        }
+
+        if (datasetValue === "false") {
+          removeInactiveClass(activeTodosCtn);
+          addInactiveClass(completedTodosCtn);
+        }
+
+        if (datasetValue === "true") {
+          removeInactiveClass(completedTodosCtn);
+          addInactiveClass(activeTodosCtn);
+        }
+      });
+    });
+  }
+
+  initNewTodoModal() {
+    const newTodoModal = document.querySelector(".modal-new");
+    const newTodoName = document.querySelector("#newTodoName");
+    const newTodoDescription = document.querySelector("#newTodoDescription");
+    const newTodoDeadline = document.querySelector("#newTodoDeadline");
+    const prioSelector = document.querySelector("#prioSelector");
+    const createNewTodoModalBtn = document.querySelector(
+      "#createNewTodoModalBtn"
+    );
+    const modalCancel = document.querySelector(".modalCancel");
+
+    console.log(newTodoName);
+    const validateForm = (fieldsForValidate) => {
+      fieldsForValidate.forEach((field) => console.log(field.value));
+    };
+
+    createNewTodoModalBtn.addEventListener("click", () => {
+      validateForm([newTodoName, newTodoDeadline, prioSelector]); // Validation need to solve
+      console.log();
+      this.lastID++;
+
+      this.list.push({
+        id: this.lastID,
+        name: newTodoName.value,
+        description: newTodoDescription.value,
+        createAt: Date.parse(this.date),
+        deadline: Date.parse(newTodoDeadline.value),
+        priority: prioSelector.value,
+        taskDone: false,
+      });
+
+      newTodoModal.classList.add("inactive");
+      this.renderList(this.list);
+    });
   }
 }
 
-const myTodo = new TodoList([
-  {
-    id: 1,
-    name: "todo1",
-    description: "todo1 leírás",
-    createAt: "2020.02.02",
-    deadline: "2022.03.01",
-    priority: 1,
-  },
-  {
-    id: 5,
-    name: "todo5",
-    description: "todo5 leírás",
-    createAt: "2020.02.07",
-    deadline: "2022.03.30",
-    priority: 1,
-  },
-  {
-    id: 2,
-    name: "todo2",
-    description: "todo2 leírás",
-    createAt: "2020.02.01",
-    deadline: "2022.03.07",
-    priority: 2,
-  },
-  {
-    id: 3,
-    name: "todo3",
-    description: "todo3 leírás",
-    createAt: "2020.02.04",
-    deadline: "2022.03.12",
-    priority: 3,
-  },
-]);
+const data = {
+  todoList: [
+    {
+      id: 1,
+      name: "todo1",
+      description: "todo1 leírás",
+      createAt: 1313174400000,
+      deadline: 1813174400000,
+      priority: 1,
+      taskDone: true,
+    },
+    {
+      id: 5,
+      name: "todo5",
+      description: "todo5 leírás",
+      createAt: 1513174400000,
+      deadline: 1813174400000,
+      priority: 1,
+      taskDone: false,
+    },
+    {
+      id: 2,
+      name: "todo2",
+      description: "todo2 leírás",
+      createAt: 1613174400000,
+      deadline: 1713174400000,
+      priority: 2,
+      taskDone: true,
+    },
+    {
+      id: 3,
+      name: "todo3",
+      description: "todo3 leírás",
+      createAt: 1234483200000,
+      deadline: 1134483200000,
+      priority: 3,
+      taskDone: false,
+    },
+  ],
+  lastID: 5,
+};
+
+const myTodo = new TodoList(data.todoList, new Date(), data.lastID);
 
 myTodo.init();
-myTodo.orderBy("priority");
+// myTodo.orderBy("priority");
 
 export default TodoList;
